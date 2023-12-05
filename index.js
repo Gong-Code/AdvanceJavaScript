22f15fda3d87f4f1bc151d7e1ec22c65c633b4e5
Gong Moonphruk <gong.moonphruk@gmail.com>
2023-12-05 21:08:36 +0100

    initial Create

---
+++ index.js
@@ -0,0 +1,57 @@
+const modal = document.getElementById('modal');
+const modalCloseBtn = document.getElementById('modal-close-btn');
+const consentForm = document.getElementById('consent-form');
+const modalText = document.getElementById('modal-text');
+
+
+
+setTimeout(() => {
+    modal.style.display = 'inline';
+}, 1500);
+
+modalCloseBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+
+consentForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const consentFormData = new FormData(consentForm);
+
+    const fullName = consentFormData.get('fullName');
+
+    const email = consentFormData.get('email');
+
+    console.log(email)
+
+
+    console.log(fullName)
+
+    modalText.innerHTML = 
+    `<div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...          
+        </p>     
+    </div>`
+
+    console.log(modalText);
+
+    setTimeout( () => {
+        document.getElementById('uploadText').innerText = `
+        Making the sale...`;
+    }, 1500);
+
+    setTimeout( () => {
+        document.querySelector('.modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+        ` ;
+    }, 3000);
+
+});
+
